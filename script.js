584d3cf821ebfa3268be81a0908598e6176cdbce
NGR <ngr112001@gmail.com>
2022-11-07 20:55:20 +0545

    commit

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const title = document.getElementByClassName('title')
-collection
+// document.getElementById('title').innerHTML = 'hello from external &ensp;'
+
+/*
+const title = document.getElementsByClassName("title");
+console.log(title)
+title[0].innerHTML = 'hello from external js &ensp;'
+*/
+
+// adding dynamic hedding in h1 using loop
+/*
+const title = document.getElementsByClassName("title");
+for (let i = 0; i < title.length; i++) {
+  title[0].innerHTML = "hello form external js with loop &ensp;";
+}
+*/
