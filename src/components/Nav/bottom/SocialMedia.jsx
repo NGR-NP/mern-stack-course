ff33e2f0f32788a738a9da28d06c73b1e2575227
NGR-NP <ngr112001@gmail.com>
2022-12-21 00:30:27 +0545

    commit

--- src/components/Nav/bottom/SocialMedia.jsx
+++ src/components/Nav/bottom/SocialMedia.jsx
@@ -12,28 +12,24 @@ const SocialMedia = () => {
           className="socialMediaIcon"
           titleAccess="Facebook"
           fontSize="medium"
-          color="action"
         ></FacebookOutlinedIcon>
 
         <InstagramIcon
           className="socialMediaIcon"
           titleAccess="Instagram"
           fontSize="medium"
-          color="action"
         ></InstagramIcon>
 
         <YouTubeIcon
           className="socialMediaIcon"
           titleAccess="Youtube"
           fontSize="medium"
-          color="action"
         ></YouTubeIcon>
 
         <WhatsAppIcon
           className="socialMediaIcon"
           titleAccess="What's app"
           fontSize="medium"
-          color="action"
         ></WhatsAppIcon>
       </div>
     </div>
