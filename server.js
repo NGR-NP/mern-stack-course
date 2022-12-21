0258361dcd77c53b8c4fd44c00433512c37859d8
NGR-NP <ngr112001@gmail.com>
2022-12-22 01:07:12 +0545

    commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const app = require("./src/config/Express");
 const { PORT } = require("./src/config/secrets");
 
 // comment this before pushing
-// const consoleMessage = require("./src/clg");
-// app.listen(PORT || 8000, () => {
-//   consoleMessage();
-// });
+const consoleMessage = require("./src/clg");
+app.listen(PORT, () => {
+  consoleMessage();
+});
 //
 
-app.listen(PORT || 8000)
+// app.listen(PORT)
--- src/config/secrets.js
+++ src/config/secrets.js
@@ -1,7 +1,7 @@
 require("dotenv").config({});
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8000,
   API_URL: process.env.API_URL,
   JWT: process.env.JWT,
   EXPIREIN: process.env.EXPIREIN,
