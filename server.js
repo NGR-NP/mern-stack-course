a336b2b0561b234de742049d2743893421f6eafe
NGR-NP <ngr112001@gmail.com>
2022-12-25 15:10:07 +0545

    commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./src/config/Express");
 const { PORT } = require("./src/config/secrets");
 
-/* reminder uncomment this line on production this is only use on development
+// /* reminder uncomment this line on production this is only use on development
 require("./src/utils/clg");
- */ //reminder uncomment this line on production this is only use on development
+//  */ //reminder uncomment this line on production this is only use on development
 
 app.listen(PORT);
