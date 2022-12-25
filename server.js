a1e3a071d49baaee5a9943425df9ea0dd2ad6753
NGR-NP <ngr112001@gmail.com>
2022-12-25 15:14:47 +0545

    commit

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const { PORT } = require("./src/config/secrets");
 require("./src/utils/clg");
  */ //reminder uncomment this line on production this is only use on development
 
-app.listen(PORT);
+app.listen(PORT, connectMongoDB());
