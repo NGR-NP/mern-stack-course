975fe61630589adfd67624643c53a3356ed01781
NGR-NP <ngr112001@gmail.com>
2022-12-27 17:15:02 +0545

    remove typo

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: "User Name is required",
       unique: true,
