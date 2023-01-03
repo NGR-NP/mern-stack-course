8829a8a36ef0763661f3163edd2f0a5b9d74c7a1
NGR-NP <ngr112001@gmail.com>
2023-01-04 00:06:47 +0545

    commit

--- src/config/Express.js
+++ src/config/Express.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", Routes);
 
