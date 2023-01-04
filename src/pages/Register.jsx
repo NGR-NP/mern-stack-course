5c30805f337ef77020d79e10a13598c8140b811c
NGR-NP <ngr112001@gmail.com>
2023-01-04 22:46:57 +0545

    commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,49 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import RegisterComp from "../components/Forms/RegisterComp";
 import "../style/Register.css";
+
 const Register = () => {
   return (
     <div className="registerCont">
       <div className="registerSec">
-        <div className="registerMain">
-          <div className="registerTitle title">Register</div>
-          <div className="registerInputCont inputCont bdrTop">
-            <label htmlFor="userName">userName</label>
-            <input
-              className="registerInput"
-              type="text"
-              placeholder="Enter your Name"
-            />
-          </div>
-          <div className="registerInputCont inputCont ">
-            <label htmlFor="email">Email</label>
-            <input
-              className="registerInput"
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter you Email"
-              autoComplete="off"
-            />
-          </div>
-          <div className="registerInputCont inputCont bdrBottom">
-            <label htmlFor="password">Password</label>
-            <input
-              className="registerInput"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Set Password"
-            />
-          </div>
-          <div className="centerADiv">
-            <button className="registerBtn">Submit</button>
-          </div>
-          <div className="infoCont">
-            <p>Already have an Account?</p>
-            <Link to="/login">Login</Link>
-          </div>
-        </div>
+        <RegisterComp />
       </div>
     </div>
   );
