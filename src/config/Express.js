fbef1ad6f04f835e378c5addd8d90b9ccb76e2b4
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-05 22:19:13 +0545

    Update Express.js

--- src/config/Express.js
+++ src/config/Express.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(cookieParser(*));
 app.use(cors());
 
 app.use("/api", Routes);
