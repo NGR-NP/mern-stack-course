6f43ead0926dcbdfb211469e62fb87550af71b5a
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-09 23:07:05 +0545

    c

--- src/models/User.js
+++ src/models/User.js
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema(
     role: {
       type: [Number],
       enum: [Admin, User],
-      default: [User, Admin],
+      default: User,
       required: "Role is required",
     },
     refreshTokenDB: String,
