e9c5313da066013c9506fb8305fa5c8ee331b75f
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-09 18:49:11 +0545

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./src/config/Express");
 const { PORT } = require("./src/config/secrets");
 
 // /* reminder only for devlopment
-require("./src/utils/clg");
+//require("./src/utils/clg");
 //  */ //reminder only for devlopment
 
 app.listen(PORT, () => {
