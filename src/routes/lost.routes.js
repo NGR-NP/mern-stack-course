4dd26ce8a9e5cd2cd831a1467e5cef50710f773a
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-12 18:22:18 +0545

    c

--- src/routes/lost.routes.js
+++ src/routes/lost.routes.js
@@ -1,6 +1,6 @@
 const routenotavailable = (_, res) => {
   res.status(404).json({
-    error: "Current path is not available ⛔",
+    message: "Current path is not available ⛔",
   });
 };
 module.exports = routenotavailable;
