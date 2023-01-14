b5250d9cf1d74bb6044fe335b6cbadae275c71b4
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-14 18:25:36 +0545

    Update Express.js

--- src/config/Express.js
+++ src/config/Express.js
@@ -8,9 +8,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const accessControl = require("../middleware/accessControl");
 const app = express();
-const allowo = {
-    origin: 'http://localhost:3000',
-}
+
 app.use(accessControl);
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
