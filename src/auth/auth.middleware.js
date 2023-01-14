692013e0a7e7417ec415450eed966c256156a9fe
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-14 15:51:50 +0545

    c

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -3,33 +3,16 @@ const { JWT } = require("../config/secrets");
 const ERROR = require("../utils/error");
 
 const verifyJwt = (req, res, next) => {
-  // const authHeader = req.headers.authorization || req.headers.Authorization;
-  // if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
-  // const token = authHeader.split(' ')[1];
-  // jwt.verify(token, JWT, (err, decoded) => {
-  //   if (err) return next(ERROR(403, "invalid token"));
-  //   req.id = decoded.id;
-  //   req.username = decoded.username;
-  //   req.role = decoded.role;
-  //   next();
-  // });
-
-
-
   const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
-    const token = authHeader.split(' ')[1];
-    console.log(token)
-    jwt.verify(
-        token,
-        JWT,
-        (err, decoded) => {
-            if (err) return next(ERROR(403, "invalid token"));;
-            req.user = decoded.username;
-            req.role = decoded.role;
-            next();
-        }
-    );
+  if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, JWT, (err, decoded) => {
+    if (err) return next(ERROR(403, "invalid token"));
+    req.id = decoded.id;
+    req.username = decoded.username;
+    req.role = decoded.role;
+    next();
+  });
 
 };
 const verifyCurrentUser = (req, res, next) => {
