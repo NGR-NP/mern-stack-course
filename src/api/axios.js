59cd2a6a0ed9fe90ce35e0d358abfe34d9b45a04
NGR-NP <ngr112001@gmail.com>
2023-01-27 20:13:40 +0545

    c

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const API_URL = "https://mern-stack-course-production.up.railway.app/api";
 // const API_URL = "https://vrit-tech-mern-project-by-tejkarki.up.railway.app/api";
+
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.accessToken;
 
