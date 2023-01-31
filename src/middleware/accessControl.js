7baae04657a9f6c0f209d00752bdb045dd047e9c
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-31 19:37:29 +0545

    cc

--- src/middleware/accessControl.js
+++ src/middleware/accessControl.js
@@ -1,11 +1,10 @@
-// const allowedOrigins = require("../utils/whitelist");
-const allowedOrigins = ["*"]
+const allowedOrigins = require("../utils/whitelist");
 
 const accessControl = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "*");
   }
   next();
 };
