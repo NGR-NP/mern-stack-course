1e169effd0f067563a7096c4e7fd8daf9f1b5b42
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-31 19:12:51 +0545

    Update Express.js

--- src/config/Express.js
+++ src/config/Express.js
@@ -10,7 +10,8 @@ const accessControl = require("../middleware/accessControl");
 const app = express();
 
 app.use(accessControl);
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
