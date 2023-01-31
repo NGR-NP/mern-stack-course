cb9f6aa646f5abb1de85876c6c0977f9de849481
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-01-31 18:59:45 +0545

    Update whitelist.js

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,6 +1,7 @@
-const { ALLOWED_DOMAIN } = require("../config/secrets");
+const { ALLOWED_DOMAIN,ALLOWED_DOMAIN_DEV } = require("../config/secrets");
 const whitelist = [
   ALLOWED_DOMAIN,
+  ALLOWED_DOMAIN_DEV,
   "http://localhost:3000",
   "http://127.0.0.1:3000",
 ];
