6fe17018c3b5f1e3b2246eb76c6885968dbce9da
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-02-24 16:38:23 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
-import ContentLoadding from "./sections/contentLoading";
+import ContentLoadding from "./sections/ContentLoading";
 
 function App() {
   return
