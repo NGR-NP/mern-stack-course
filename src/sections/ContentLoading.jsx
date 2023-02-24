bb1f347c6cb866abac046c19f2435f2599852c9f
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-02-24 17:05:32 +0545

    cc

--- src/sections/ContentLoading.jsx
+++ src/sections/ContentLoading.jsx
@@ -1,5 +1,45 @@
-import React, { useState, useEffect } from "react";
-import Circle from "../components/loading/Circle";
+const ring = keyframes`
+0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+const Main = styled.div`
+  position: fixed;
+  margin-top: 2.25rem;
+  width: 100vw;
+  color: rgb(209 213 219 / 1);
+  z-index: 9999;
+`
+const Sec = styled.div`
+display: inline-block;
+position: relative;
+width: 5rem
+height: 5rem;
+`
+const Circle = styled.div`
+    box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 22px;
+  height: 22px;
+  margin: 2px;
+  border: 2px solid #34ff01;
+  border-radius: 50%;
+  animation: ${ring} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #34ff01 transparent transparent transparent;
+  :nth-child(1) {
+  animation-delay: -0.45s;
+ }
+ :nth-child(2) {
+  animation-delay: -0.3s;
+ }
+ :nth-child(3) {
+  animation-delay: -0.15s;
+ }
+`
 const ContentLoadding = () => {
     const [loading, setLoading] = useState(true);
 
@@ -13,9 +53,16 @@ const ContentLoadding = () => {
 
     return (
         <>
-            {loading &&<p>test</p>
+            {loading &&
+                <Main>
+                    <Sec>
+                        <Circle />
+                        <Circle />
+                        <Circle />
+                        <Circle />
+                    </Sec>
+                </Main>
             }
-                <Circle top="20px"/>
         </>
     );
 };
