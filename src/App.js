cdd44118962e08fee8a9e7e9081ac47d051c4237
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-02-24 16:46:27 +0545

    cc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
-// import ContentLoadding from "./sections/ContentLoading";
+import ContentLoadding from "./sections/ContentLoading";
 
 function App() {
-  return
-  (
+  return (
     <>
-      {/* <ContentLoadding /> */}
+      <ContentLoadding />
       <RouterProvider router={router} />;
     </>
   )
