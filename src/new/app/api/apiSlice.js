12f0e9235022756645e70929cc1efb795cfd914d
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-03-22 09:18:47 +0545

    commit

--- src/new/app/api/apiSlice.js
+++ src/new/app/api/apiSlice.js
@@ -37,62 +37,74 @@ import { showToastMessage } from "../../custonToast/toastSlice";
 
 //   return result;
 // };
+const tryBaseUrls = async (baseUrls, options) => {
+  for (const baseUrl of baseUrls) {
+    try {
+      const result = await fetchBaseQuery({
+        ...options,
+        baseUrl,
+      })("/");
 
-const baseUrls = [
-  "https://mern-stack-course-production.up.railway.app/api",
-  // "https://vrit-tech-mern-project-by-tejkarki.up.railway.app/api",
-];
-
-let currentBaseUrlIndex = 0;
-
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions);
-
-  if (result?.error?.data?.status === 403) {
-    console.log("sending refresh token");
-    const refreshResult = await baseQuery("/refresh", api, extraOptions);
-
-    if (refreshResult?.data) {
-      const username = api.getState().auth.username;
-      api.dispatch(setCredentials({ ...refreshResult.data, username }));
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logOut());
-      api.dispatch(
-        showToastMessage({ message: "session expired!", type: "error" })
-      );
+      if (result?.status === 200) {
+        return baseUrl;
+      }
+    } catch (e) {
+      console.log(`Failed to fetch from ${baseUrl}`, e);
     }
   }
-
-  if (result?.error?.data?.status === 503) {
-    currentBaseUrlIndex = (currentBaseUrlIndex + 1) % baseUrls.length;
-    const nextBaseUrl = baseUrls[currentBaseUrlIndex];
-    console.log(`Switching to ${nextBaseUrl}`);
-
-    const nextBaseQuery = fetchBaseQuery({
-      baseUrl: nextBaseUrl,
-      credentials: "include",
-      prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token;
-        if (token) {
-          headers.set("authorization", `Bearer ${token}`);
-        }
-        return headers;
-      },
-    });
-
-    result = await nextBaseQuery(args, api, extraOptions);
-
-    if (result?.error?.data?.status === 503 && currentBaseUrlIndex === baseUrls.length - 1) {
-      console.log(`Both URLs are down. Sending email to ngr112001@gmail.com`);
-
-
-      currentBaseUrlIndex = 0; 
-    }
-  }
-
-  return result;
+  return null;
 };
+
+const baseUrl = await tryBaseUrls(
+  ["https://mern-stack-course-production.up.railway.app/api", "https://vrit-tech-mern-project-by-tejkarki.up.railway.app/api"],
+  {
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }
+);
+
+if (baseUrl) {
+  const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await fetchBaseQuery({
+      ...extraOptions,
+      baseUrl,
+    })(args, api);
+
+    if (result?.error?.data?.status === 403) {
+      console.log("sending refresh token");
+      const refreshResult = await fetchBaseQuery({
+        ...extraOptions,
+        baseUrl,
+      })("/refresh", api);
+
+      if (refreshResult?.data) {
+        const username = api.getState().auth.username;
+        api.dispatch(setCredentials({ ...refreshResult.data, username }));
+        result = await fetchBaseQuery({
+          ...extraOptions,
+          baseUrl,
+        })(args, api);
+      } else {
+        api.dispatch(logOut());
+        api.dispatch(
+          showToastMessage({
+            message: "session expired!",
+            type: "error",
+          })
+        );
+      }
+    }
+
+    return result;
+  };
+}
+
 
 
 export const apiSlice = createApi({
